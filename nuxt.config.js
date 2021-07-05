9e1e6556702963fbe034e3df5468e36408e22b97
abedigram <mbapost@yahoo.com>
2021-07-05 15:15:28 +0430

    static route changed

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,9 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  router: {
+    base: '/tmp-landing/'
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'nahrein',
